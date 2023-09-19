2a64516f27899a77ad46ace2a8db7c2080c41715
Matheus F. <matheusfreitas.a09@gmail.com>
2023-09-19 13:26:09 -0300

    Atualização do código do contato

--- pages/home/contato.js
+++ pages/home/contato.js
@@ -1,65 +1,66 @@
-class FormSubmit {
-    constructor(settings) {
-      this.settings = settings;
-      this.form = document.querySelector(settings.form);
-      this.formButton = document.querySelector(settings.button);
-      if (this.form) {
-        this.url = this.form.getAttribute("action");
-      }
-      this.sendForm = this.sendForm.bind(this);
+class EnviarFormulario {
+  constructor(configuracoes) {
+    this.configuracoes = configuracoes;
+    this.formulario = document.querySelector(configuracoes.form);
+    this.botaoFormulario = document.querySelector(configuracoes.button);
+    if (this.formulario) {
+      this.url = this.formulario.getAttribute("action");
     }
-  
-    displaySuccess() {
-      this.form.innerHTML = this.settings.success;
-    }
-  
-    displayError() {
-      this.form.innerHTML = this.settings.error;
-    }
-  
-    getFormObject() {
-      const formObject = {};
-      const fields = this.form.querySelectorAll("[name]");
-      fields.forEach((field) => {
-        formObject[field.getAttribute("name")] = field.value;
+    this.enviarFormulario = this.enviarFormulario.bind(this);
+  }
+
+  exibirSucesso() {
+    this.formulario.innerHTML = this.configuracoes.sucesso;
+  }
+
+  exibirErro() {
+    this.formulario.innerHTML = this.configuracoes.erro;
+  }
+
+  obterObjetoFormulario() {
+    const objetoFormulario = {};
+    const campos = this.formulario.querySelectorAll("[name]");
+    campos.forEach((campo) => {
+      objetoFormulario[campo.getAttribute("name")] = campo.value;
+    });
+    return objetoFormulario;
+  }
+
+  duranteEnvio(event) {
+    event.preventDefault();
+    event.target.disabled = true;
+    event.target.innerText = "Enviando...";
+  }
+
+  async enviarFormulario(event) {
+    try {
+      this.duranteEnvio(event);
+      await fetch(this.url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(this.obterObjetoFormulario()),
       });
-      return formObject;
-    }
-  
-    onSubmission(event) {
-      event.preventDefault();
-      event.target.disabled = true;
-      event.target.innerText = "Enviando...";
-    }
-  
-    async sendForm(event) {
-      try {
-        this.onSubmission(event);
-        await fetch(this.url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify(this.getFormObject()),
-        });
-        this.displaySuccess();
-      } catch (error) {
-        this.displayError();
-        throw new Error(error);
-      }
-    }
-  
-    init() {
-      if (this.form) this.formButton.addEventListener("click", this.sendForm);
-      return this;
+      this.exibirSucesso();
+    } catch (erro) {
+      this.exibirErro();
+      throw new Error(erro);
     }
   }
-  
-  const formSubmit = new FormSubmit({
-    form: "[data-form]",
-    button: "[data-button]",
-    success: "<h1 class='success'>Mensagem enviada!</h1>",
-    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
-  });
-  formSubmit.init();
+
+
+  iniciar() {
+    if (this.formulario) this.botaoFormulario.addEventListener("click", this.enviarFormulario);
+    return this;
+  }
+}
+
+const formularioEnviar = new EnviarFormulario({
+  form: "[data-form]",
+  button: "[data-button]",
+  sucesso: "<h1 class='sucesso'>Mensagem enviada!</h1>",
+  erro: "<h1 class='erro'>Não foi possível enviar sua mensagem.</h1>",
+});
+formularioEnviar.iniciar();
